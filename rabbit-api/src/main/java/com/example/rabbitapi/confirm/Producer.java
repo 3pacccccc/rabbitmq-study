63ca4a270d7ad92635d78c6da6b4295792199cdd
3pacccccc <351489917@qq.com>
2019-12-24 00:00:54 +0800

    3-5 08:34

--- rabbit-api/src/main/java/com/example/rabbitapi/confirm/Producer.java
+++ rabbit-api/src/main/java/com/example/rabbitapi/confirm/Producer.java
@@ -1,4 +1,27 @@
 package com.example.rabbitapi.confirm;
 
+import com.example.rabbitapi.utils.Connect2Rabbitmq;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class Producer {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //1.创建一个connectionFactory
+        Connection connection = Connect2Rabbitmq.getConnection();
+        Channel channel = connection.createChannel();
+
+        //2.指定消息投递模式：消息的确认模式
+        channel.confirmSelect();
+
+        String exchangeName = "test_confirm_exchange";
+        String routingKey = "confirm.save";
+
+        // 3. 发送一条消息
+        String msg = "hello rabbitmq send confirm message";
+        channel.basicPublish(exchangeName, routingKey, null, msg.getBytes());
+
+    }
 }
