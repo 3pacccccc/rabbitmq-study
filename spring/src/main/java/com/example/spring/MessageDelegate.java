ea6957b509ac051ddef61f93a50103650ba4d4b7
马锐敏 <351489917@qq.com>
2019-12-27 18:31:48 +0800

    4-8完成

--- spring/src/main/java/com/example/spring/MessageDelegate.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring;
-
-public class MessageDelegate {
-
-    public void handleMessage(byte[] messageBody) {
-
-        System.err.println("默认方法，消息内容：" + new String(messageBody));
-    }
-}
